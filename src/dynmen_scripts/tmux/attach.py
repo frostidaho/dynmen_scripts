0c0bff7b5302edfd369ef6470db7e8e274817d66
Idaho Frost <frostidaho@gmail.com>
2017-03-29 23:25:50 -0400

    Started moving tmux_sessions to its own package

--- src/dynmen_scripts/tmux/attach.py
+++ src/dynmen_scripts/tmux/attach.py
@@ -1,34 +1,34 @@
-from contextlib import contextmanager
-from subprocess import run
+from contextlib import contextmanager as _contextmanager
+from subprocess import run as _run
 
-tty_script_template = """
+_tty_script_template = """
 #!/usr/bin/sh
 tmux source '{tmux_commands_path}'
 #{shell}
 """
 
-tmux_commands_template = '''
+_tmux_commands_template = '''
 attach -t "{session_id}"
 select-window -t "{window_index}"
 select-pane -t {pane_index}
 '''
 
-@contextmanager
-def attach_script(pane_info):
+@_contextmanager
+def _attach_script(pane_info):
     from tempfile import TemporaryDirectory
     from stat import S_IEXEC
-    from os import getenv, chmod, stat
+    from os import getenv, chmod, stat, path
 
     with TemporaryDirectory() as td:
         fpath = path.join(td, 'torun.tmux')
         fpath_torun = path.join(td, 'torun')
 
         with open(fpath, mode='w') as fscript:
-            tmux_commands = tmux_commands_template.format(**pane_info._asdict())
+            tmux_commands = _tmux_commands_template.format(**pane_info._asdict())
             fscript.write(tmux_commands)
 
         with open(fpath_torun, mode='w') as fscript:
-            ttyscript = tty_script_template.format(
+            ttyscript = _tty_script_template.format(
                 tmux_commands_path=fpath,
                 shell=getenv('SHELL', 'bash'),
             )
@@ -38,13 +38,20 @@ def attach_script(pane_info):
         yield fpath_torun
 
 
+# _guake_template = """
+# #!/usr/bin/sh
+# guake --select-tab 0\
+#       --show\
+#       --execute-command {script_path}
+# """
+
 class Attach:
     def __init__(self, backend='xfce4'):
         self.backend = backend
 
     def __call__(self, pane_info):
         fn = getattr(self, 'run_in_{}'.format(self.backend))
-        with attach_script(pane_info) as script_path:
+        with _attach_script(pane_info) as script_path:
             return fn(script_path)
 
     @staticmethod
@@ -55,5 +62,17 @@ class Attach:
             '--maximize',
             '--command={}'.format(script_path),
         ]
-        return run(cmd)
+        return _run(cmd)
+
+    @staticmethod
+    def run_in_guake(script_path):
+        cmd = [
+            'guake',
+            '--select-tab',
+            '0',
+            '--show',
+            '--execute-command',
+            script_path,
+        ]
+        return _run(cmd)
 
--- src/dynmen_scripts/tmux_sessions.py
+++ src/dynmen_scripts/tmux_sessions.py
@@ -40,51 +40,10 @@ def get_panes():
     make = PaneInfo._make
     return [make(x) for x in lines]
 
-tty_script_template = """
-#!/usr/bin/sh
-tmux source '{tmux_commands_path}'
-#{shell}
-"""
-
-tmux_commands_template = '''
-attach -t "{session_id}"
-select-window -t "{window_index}"
-select-pane -t {pane_index}
-'''
-
-@contextmanager
-def attach_script(pane_info):
-    from tempfile import TemporaryDirectory
-    from stat import S_IEXEC
-    from os import getenv, chmod, stat
-
-    with TemporaryDirectory() as td:
-        fpath = path.join(td, 'torun.tmux')
-        fpath_torun = path.join(td, 'torun')
-
-        with open(fpath, mode='w') as fscript:
-            tmux_commands = tmux_commands_template.format(**pane_info._asdict())
-            fscript.write(tmux_commands)
-
-        with open(fpath_torun, mode='w') as fscript:
-            ttyscript = tty_script_template.format(
-                tmux_commands_path=fpath,
-                shell=getenv('SHELL', 'bash'),
-            )
-            fscript.write(ttyscript)
-        st = stat(fpath_torun)
-        chmod(fpath_torun, st.st_mode | S_IEXEC)
-        yield fpath_torun
-
 def attach(pane_info):
-    with attach_script(pane_info) as script_path:
-        cmd = [
-            'xfce4-terminal',
-            '--show-borders',
-            '--maximize',
-            '--command={}'.format(script_path),
-        ]
-        return run(cmd)
+    from .tmux.attach import Attach
+    a = Attach(backend='guake')
+    return a(pane_info)
 
 session_template = OrderedDict()
 session_template['Session'] = '{session_name} ({session_id})'
