91c19202877be178ebf16a558e5b901bcf81361f
Idaho Frost <frostidaho@gmail.com>
2017-04-02 15:33:15 -0400

    Move Register to common.py

--- src/dynmen_scripts/tmux/terminal.py
+++ src/dynmen_scripts/tmux/terminal.py
@@ -1,43 +1,8 @@
-from weakref import WeakValueDictionary as _WeakValueDictionary
-from weakref import WeakSet as _WeakSet
-from .common import NO_PANE, FileInfo
+from .common import NO_PANE, FileInfo, Register
 from itertools import chain
 import subprocess as _sp
 from shutil import which as _which
 from functools import partial as _partial
-
-class Register:
-    def __new__(cls, *args, **kwargs):
-        instance = object.__new__(cls)
-        if "_instances" not in cls.__dict__:
-            cls._instances = _WeakSet()
-        cls._instances.add(instance)
-        return instance
-
-    def __init__(self, name):
-        self.name = name
-        self.registered = _WeakValueDictionary()
-        self.default = None
-
-    def __getitem__(self, key):
-        return self.registered[key]
-
-    def __call__(self, fn, name='', default=False):
-        if not name:
-            name = fn.__name__
-        if self.default is None:
-            self.default = name
-        elif default:
-            self.default = name
-        self.registered[name] = fn
-        return fn
-
-    def __repr__(self):
-        cname = self.__class__.__name__
-        name = self.name
-        dflt = self.default
-        return '<{cname}({name!r}): default -> {dflt}>'.format(**locals())
-
 
 register_terminal = Register('terminal')
 
