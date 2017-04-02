a817f38f05a12ae0c989857322b349f0bd4d2086
Idaho Frost <frostidaho@gmail.com>
2017-04-02 14:34:34 -0400

    Add docstrings to ctrl.py

--- src/dynmen_scripts/tmux/ctrl.py
+++ src/dynmen_scripts/tmux/ctrl.py
@@ -9,6 +9,7 @@ def _make_get_panes():
     run, PIPE, DEVNULL = _sp.run, _sp.PIPE, _sp.DEVNULL
     make = PaneInfo._make
     def get_tmux_panes():
+        "Return a PaneInfo object for each available tmux pane"
         lines = run(cmd, stdout=PIPE, stderr=DEVNULL, stdin=DEVNULL) \
                                  .stdout.decode().splitlines()
         return [make(x.split(sep)) for x in lines]
@@ -17,12 +18,18 @@ get_tmux_panes = _make_get_panes()
 
 
 def kill_pane(pane_info):
+    "Kill the tmux pane corresponding to the given instance of PaneInfo"
     d = pane_info._asdict()
     fmt = '{session_id}:{window_index}.{pane_index}'
     cmd = ['tmux', 'kill-pane', '-t', fmt.format(**d)]
     return _sp.run(cmd)
 
 def new_session(name, systemd=False):
+    """Create a new tmux session.
+
+    If systemd is True then create the new tmux session
+    by using 'systemd-run --scope --user'.
+    """
     cmd = ['tmux', 'new-session', '-d', '-s', name]
     if systemd:
         systemd_cmd = ['systemd-run', '--scope', '--user']
@@ -30,7 +37,3 @@ def new_session(name, systemd=False):
         return _sp.run(systemd_cmd)
     return _sp.run(cmd)
 
-
-# if __name__ == '__main__':
-#     from timeit import timeit
-#     print('get_tmux_panes', timeit('x = get_tmux_panes()', 'from __main__ import get_tmux_panes', number=1000))
